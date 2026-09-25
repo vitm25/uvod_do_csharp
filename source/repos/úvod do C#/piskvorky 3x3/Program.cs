76b3050ab76ef69c0c64cc6eb4b73a0a679d082f
Marek Vít <vitm25@spstrutnov.cz>
2026-09-25 11:55:56 +0200

    upravy

---
+++ source/repos/úvod do C#/piskvorky 3x3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace piskvorky_3x3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Piškvorkyyyyyyyyyyyy");
+        }
+    }
+}
