a723628522766f0d69c09e55df4585f9dac4a81d
Marek Vít <vitm25@spstrutnov.cz>
2026-09-25 12:12:20 +0200

    pokracovani na piskrovkach

--- source/repos/úvod do C#/piskvorky 3x3/Program.cs
+++ source/repos/úvod do C#/piskvorky 3x3/Program.cs
@@ -5,6 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Piškvorkyyyyyyyyyyyy");
+            string[] pole = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+            string hrac = "X";
+            int tahy = 0;
+
+            while (true)
+            {
+                //pole
+                Console.WriteLine(pole[0] + "|" + pole[1] + "|" + pole[2]);
+                Console.WriteLine(pole[3] + "|" + pole[4] + "|" + pole[5]);
+                Console.WriteLine(pole[6] + "|" + pole[7] + "|" + pole[8]);
+
+                //tah hrace
+                Console.Write("hraje " + hrac + ", zadej číslo políčka: ");
+                int cislo = Convert.ToInt32(Console.ReadLine());
+
+                if (cislo < 1 || cislo > 9 || pole[cislo - 1] == "X" || pole[cislo - 1] == "0");
+                {
+                    Console.WriteLine("tam nejde hrát, zkus to znovu");
+                    continue;
+                }
+
+            }
         }
     }
 }
